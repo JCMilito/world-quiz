a975e3e6457698af0b9e6d24685a2185cb511de0
JCMilito <jcm2112@gmail.com>
2021-05-27 11:52:30 -0300

    Beta Version

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
-import React, { MouseEventHandler, ReactNode } from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core';
+import React, { MouseEventHandler, ReactNode } from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core";
 
 type Props = {
   color: string;
@@ -13,24 +13,47 @@ export default function CustomButton({
   handleClick,
   children,
 }: Props): JSX.Element {
-  const useStyles = makeStyles(() => ({
-    button: {
-      width: '250px',
-      height: '60px',
-      borderRadius: '8px',
-      margin: '2px',
-      backgroundColor: color,
-      fontSize: '12px',
-      fontWeight: 'bold',
-      color: 'white',
-    },
-  }));
+  let useStyles;
+
+  
+  console.log(screen.width);
+
+  if (screen.width > 600) {    
+    useStyles = makeStyles(() => ({
+      button: {
+        width: "250px",
+        height: "60px",
+        borderRadius: "8px",
+        margin: "2px",
+        backgroundColor: color,
+        fontSize: "12px",
+        fontWeight: "bold",
+        color: "white",
+      },
+    }));
+  } else {    
+    useStyles = makeStyles(() => ({
+      button: {
+        width: "100%",
+        height: "60px",
+        borderRadius: "8px",
+        margin: "2px",
+        backgroundColor: color,
+        fontSize: "12px",
+        fontWeight: "bold",
+        color: "white",
+        "&:hover": {
+          backgroundColor: color
+      }
+      },
+    }));
+  }
 
   return (
     <div>
       <Button
-        variant='contained'
-        size='large'
+        variant="contained"
+        size="large"
         className={useStyles().button}
         onClick={handleClick}
       >
